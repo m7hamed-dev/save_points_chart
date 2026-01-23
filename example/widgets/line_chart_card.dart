56bb5ac963872adf76a2e80c8fe678a2dcb1b7f8
mohamed syed <moh94syed@gmail.com>
2026-01-23 20:03:11 +0300

    Update padding in LineChartCard for improved layout consistency

--- example/widgets/line_chart_card.dart
+++ example/widgets/line_chart_card.dart
@@ -10,7 +10,7 @@ class LineChartCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(6),
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
