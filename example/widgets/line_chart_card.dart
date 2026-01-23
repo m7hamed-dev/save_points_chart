a788de5d81aff603ebfd0cce4b1d66423bdc5dfd
mohamed syed <moh94syed@gmail.com>
2026-01-23 20:01:00 +0300

    Update dependencies and adjust padding in LineChartCard for improved layout
    - Bumped version of local package to 1.5.2 in pubspec.lock.
    - Reduced padding in LineChartCard from 16 to 6 for a more compact design.

--- example/widgets/line_chart_card.dart
+++ example/widgets/line_chart_card.dart
@@ -10,7 +10,7 @@ class LineChartCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(6),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
